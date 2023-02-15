4e844a0d628c81ba6455275407354e9677a511ef
Dmytro Martus <funbrainfun@gmail.com>
2023-02-15 14:25:11 +0200

    first page non resposive

--- js/main.js
+++ js/main.js
@@ -17,4 +17,36 @@ $(function () {
         $(this).addClass('search__tabs-item--active');
         $('#tab-' + `${$(this).data('id')}`).addClass('search__content-item--active');
     });
+
+    $('.product-item__favorite').on('click', function (e) {
+        $(this).toggleClass('product-item__favorite--active');
+    });
+
+    $('.tab').on('click', function (e) {
+        e.preventDefault();
+        $($(this).siblings()).removeClass('tab--active');
+        $(this).addClass('tab--active');
+        let data = e.target.dataset.id;
+        data = '#' + data;
+        $($(data).siblings()).removeClass('tabs-content--active');
+        $(data).addClass('tabs-content--active');
+        $('.product-slider').slick('unslick');
+        $('.product-slider').slick({
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            prevArrow:
+                '<button class="product-slider__btn product-slider__btn--prev"><img src="images/icon/slider-arrow-bl_l.svg" alt=""></button>',
+            nextArrow:
+                '<button class="product-slider__btn product-slider__btn--next"><img src="images/icon/slider-arrow-bl_r.svg" alt=""></button>',
+        });
+    });
+
+    $('.product-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow:
+            '<button class="product-slider__btn product-slider__btn--prev"><img src="images/icon/slider-arrow-bl_l.svg" alt=""></button>',
+        nextArrow:
+            '<button class="product-slider__btn product-slider__btn--next"><img src="images/icon/slider-arrow-bl_r.svg" alt=""></button>',
+    });
 });
